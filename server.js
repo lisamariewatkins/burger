66b5df85561910192e87699f8ef862f44587e037
Lisa Watkins <Lisa Watkins>
2016-07-26 10:18:47 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+//override POST method
+app.use(methodOverride('_method'));
+//set up handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//link routes
+var routes = require('./controllers/burger_controller');
+
+var PORT = 
+app.listen(PORT, function(){
+	console.log("Listening at Port " + PORT)
+})
