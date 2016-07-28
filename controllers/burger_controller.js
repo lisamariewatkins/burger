7cd48f0467ef24de62551a8f28fce328578e57ef
Lisa Watkins <Lisa Watkins>
2016-07-28 18:04:42 -0500

    fixed silly mistake

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -19,7 +19,7 @@ router.post('/create', function(req,res){
 	});
 });
 
-router.put('burgers/update/:id', function(req, res){
+router.post('update/:id', function(req, res){
 	var condition = "id = " + req.params.id;
 
 	burger.updateOne({'devoured':req.body.devoured}, condition, function(data){
